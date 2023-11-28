b39b03209bed73e69abd021aa7c50d944ec0eafb
AnnaK <njutcka@yandex.ru>
2023-11-28 10:14:45 +0700

    Initial commit

---
+++ catalog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from catalog.apps import CatalogConfig
+
+from catalog.views import contacts, home
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('', home, name= 'home'),
+    path('contacts/', contacts, name= 'contacts'),
+
+]
